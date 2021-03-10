6752a4ace3223b2d9fe45ce3962f8a14312d161f
Christine Yeh <christineyyeh@gmail.com>
2021-03-09 20:47:53 -0800

    ammending installation

--- install.R
+++ install.R
@@ -1,19 +1,19 @@
 install.packages(
   c(
-    'Seurat',
-    'qgraph', 
-    'data.table', 
-    'igraph', 
-    'glasso', 
-    'tidyverse',
-    'OneR', 
-    'Matrix', 
-    'circlize'
-    'preprocessCore'
-    'viridis'
-    'clusterProfiler'
-    'networkD3'
-    'org.Hs.eg.db',
-    'msigdbr'
+	'Seurat',
+	'qgraph', 
+	'data.table', 
+	'igraph', 
+	'glasso', 
+	'tidyverse',
+	'OneR', 
+	'Matrix', 
+	'circlize'
+	'preprocessCore'
+	'viridis'
+	'networkD3'
+	'BiocManager'
   )
 )
+
+BiocManager::install(c('org.Hs.eg.db','msigdbr', 'clusterProfiler'))
